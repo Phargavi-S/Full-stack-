56d7f901ab0614072ae767c5d4c347475583ac02
Phargavi-S <phargavi102005@gmail.com>
2025-02-15 18:11:56 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,25 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  let name="PHARGAVI S"
-  const details={
-    name:"PHARGAVI S",
-    age:19,
-    roll:14
-  }
-  alert("Hi Welcome ")
- console.log('My name is ',details.name)
-
+const App = () => {
+  const myName = "Phargavi s";
+  const myAge = 19;
+  React.useEffect(() => {
+    alert("Hi");
+    console.log(`My age is ${myAge}`);
+  }, []);
 
   return (
-    <>
-      <h2 style={{textAlign:"center"}}>Inline css</h2>
-      <h1 style={{textAlign:"center",color:"Blue"}}>PHARGAVI S</h1><br></br>
-      <h2 style={{textAlign:'center'}}>External css</h2>
-      <p className='info'>Name : {details.name}</p>
-    
-    </>
-  )
-}
+    <div className="name-container">
+      {myName}
+    </div>
+  );
+};
+
+export default App;
