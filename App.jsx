fea471dec502d7d4c7f6ee97e36fa2e9fcc91796
Phargavi-S <phargavi102005@gmail.com>
2025-02-15 10:02:23 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -4,13 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let name="PHARGAVI S"
+  const details={
+    name:"PHARGAVI S",
+    age:19,
+    roll:14
+  }
+  alert("Hi Welcome ")
+ console.log('My name is ',details.name)
+
 
   return (
     <>
-      <h1>PHARGAVI S</h1>
+      <h2 style={{textAlign:"center"}}>Inline css</h2>
+      <h1 style={{textAlign:"center",color:"Blue"}}>PHARGAVI S</h1><br></br>
+      <h2 style={{textAlign:'center'}}>External css</h2>
+      <p className='info'>Name : {details.name}</p>
+    
     </>
   )
 }
-
-export default App
